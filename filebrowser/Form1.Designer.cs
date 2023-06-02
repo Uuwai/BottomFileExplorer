4b40b5c0e542a962d5b56ddb40094715f7ee0e38
ウウワイ <118117530+Uuwai@users.noreply.github.com>
2023-06-02 18:08:35 +0800

    Add files via upload

---
+++ filebrowser/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace filebrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.resizer = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.runOnStartupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "file-icon.png");
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(0, 46);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.ScrollBarsEnabled = false;
+            this.webBrowser1.Size = new System.Drawing.Size(500, 453);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("C:\\Users\\Kapi\\Desktop", System.UriKind.Absolute);
+            // 
+            // resizer
+            // 
+            this.resizer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.resizer.ContextMenuStrip = this.contextMenuStrip1;
+            this.resizer.Cursor = System.Windows.Forms.Cursors.SizeNS;
+            this.resizer.Dock = System.Windows.Forms.DockStyle.Top;
+            this.resizer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.resizer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resizer.Location = new System.Drawing.Point(0, 0);
+            this.resizer.Name = "resizer";
+            this.resizer.Size = new System.Drawing.Size(500, 21);
+            this.resizer.TabIndex = 1;
+            this.resizer.Text = "⇕";
+            this.resizer.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.resizer.MouseDown += new System.Windows.Forms.MouseEventHandler(this.resizer_MouseDown);
+            this.resizer.MouseMove += new System.Windows.Forms.MouseEventHandler(this.resizer_MouseMove);
+            this.resizer.MouseUp += new System.Windows.Forms.MouseEventHandler(this.resizer_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(0, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(10, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "＜";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(490, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(10, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "＞";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.runOnStartupToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(156, 26);
+            // 
+            // runOnStartupToolStripMenuItem
+            // 
+            this.runOnStartupToolStripMenuItem.Name = "runOnStartupToolStripMenuItem";
+            this.runOnStartupToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.runOnStartupToolStripMenuItem.Text = "Run On Startup";
+            this.runOnStartupToolStripMenuItem.Click += new System.EventHandler(this.runOnStartupToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 500);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.resizer);
+            this.Controls.Add(this.webBrowser1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(21, 21);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label resizer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem runOnStartupToolStripMenuItem;
+    }
+}
+
---
+++ filebrowser/Form1.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.Win32;
+using Microsoft.Win32.TaskScheduler;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace filebrowser
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        static readonly IntPtr HWND_BOTTOM = new IntPtr(1);
+        const UInt32 SWP_NOSIZE = 0x0001;
+        const UInt32 SWP_NOMOVE = 0x0002;
+        const UInt32 SWP_NOACTIVATE = 0x0010;
+        public Form1()
+        {
+            InitializeComponent();
+
+            SetWindowPos(Handle, HWND_BOTTOM, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE);
+            this.Size = new Size(Screen.PrimaryScreen.Bounds.Width / 2,21);
+            this.Location = new Point((Screen.PrimaryScreen.Bounds.Width/2)-(this.Size.Width/2), Screen.PrimaryScreen.WorkingArea.Bottom- 21);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.SendToBack();
+            TaskService taskService = new TaskService();
+            Microsoft.Win32.TaskScheduler.Task task = taskService.FindTask("bottomfilebrowser");
+            if (task != null)
+            {
+                runOnStartupToolStripMenuItem.Checked = true;
+            }
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            this.SendToBack();
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            this.SendToBack();
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            this.SendToBack();
+        }
+        bool md = false;
+        private void resizer_MouseUp(object sender, MouseEventArgs e)
+        {
+            md = false;
+        }
+
+        private void resizer_MouseDown(object sender, MouseEventArgs e)
+        {
+            md = true;
+        }
+
+        private void resizer_MouseMove(object sender, MouseEventArgs e)
+        {
+            Rectangle workingArea = Screen.PrimaryScreen.WorkingArea;
+            if (md == true)
+            {
+                this.Top = Math.Min(Screen.PrimaryScreen.WorkingArea.Bottom - 21, Math.Max(Screen.PrimaryScreen.WorkingArea.Top,Cursor.Position.Y));
+                this.Height = Screen.PrimaryScreen.Bounds.Height-this.Top;
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void runOnStartupToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                TaskService taskService = new TaskService();
+                Microsoft.Win32.TaskScheduler.Task task = taskService.FindTask("bottomfilebrowser");
+                if (task != null)
+                {
+                    runOnStartupToolStripMenuItem.Checked = true;
+                }
+                if (runOnStartupToolStripMenuItem.Checked == false)
+                {
+                    if (task == null)
+                    {
+                        runOnStartupToolStripMenuItem.Checked = true;
+
+                        TaskDefinition taskDefinition = taskService.NewTask();
+
+                        taskDefinition.RegistrationInfo.Description = "start up";
+                        taskDefinition.Principal.RunLevel = TaskRunLevel.Highest;
+                        taskDefinition.Triggers.Add(new LogonTrigger());
+
+                        taskDefinition.Actions.Add(new ExecAction(Application.ExecutablePath));
+
+                        taskService.RootFolder.RegisterTaskDefinition("bottomfilebrowser", taskDefinition);
+
+                    }
+                    runOnStartupToolStripMenuItem.Checked = true;
+                }
+                else
+                {
+                    if (task != null)
+                    {
+                        taskService.RootFolder.DeleteTask("bottomfilebrowser");
+                    }
+                    taskService.Dispose();
+                    runOnStartupToolStripMenuItem.Checked = false;
+                }
+            }
+            catch { runOnStartupToolStripMenuItem.Checked = false; MessageBox.Show("Restart as admin","",MessageBoxButtons.OK,MessageBoxIcon.Error); }
+        }
+    }
+}
